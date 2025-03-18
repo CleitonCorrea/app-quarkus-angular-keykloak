ca588e6e7d9ea710397d68a272162c703db27490
Cleiton <cleiton.correa@compasso.com.br>
2025-03-18 17:32:49 -0300

    corrigindo bug na inicialização do keycloak

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideKeycloak } from 'keycloak-angular';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideKeycloak({
+    config: {
+      url: 'http://localhost:8188',
+      realm: 'app-quarkus-angular-keycloak',
+      clientId: 'frontend'
+    },
+    initOptions: {
+      onLoad: 'check-sso',
+      silentCheckSsoRedirectUri: './assets/silent-check-sso.html'
+    }
+  }),
+  provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideClientHydration(withEventReplay(),)]
 };
